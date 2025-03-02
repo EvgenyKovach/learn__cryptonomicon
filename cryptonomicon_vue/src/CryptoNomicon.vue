04bdca2d09dfdb851b7bd0028260d5df2b150141
FlakLP <zheka.ae@mail.ru>
2025-03-02 17:48:28 +0500

    test 2

--- cryptonomicon_vue/src/CryptoNomicon.vue
+++ cryptonomicon_vue/src/CryptoNomicon.vue
@@ -24,7 +24,7 @@
     <!--        ></path>-->
     <!--      </svg>-->
     <!--    </div>-->
-    <div class="container test">
+    <div class="container">
       <section>
         <div class="flex">
           <div class="max-w-xs">
